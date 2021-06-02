db9ff1fa0a62064e9726b9dcd289081bb5fc3069
Niklas Moss <nin@envidan.dk>
2021-06-02 11:20:21 +0200

    Small text work however I'll wait until I have a professional designer to help me

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import Headline from './components/Headline';
 import InformationSection from './components/InformationSection';
 
 function App() {
   return (
     <div className="App">
-      <InformationSection backgroundImage="coffee.jpg" />
+      <InformationSection backgroundImage="coffee.jpg">
+        <Headline text="Moss Consulting" />
+      </InformationSection>
       <InformationSection backgroundImage="nature.jpg" />
       <InformationSection backgroundImage="waterfall.jpg" />
     </div>
