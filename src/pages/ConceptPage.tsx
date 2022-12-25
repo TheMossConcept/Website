f01a3204fbfec3b02fb4449d322d2a40d4441f9c
Niklas Moss <niklas@themossconcept.com>
2022-12-25 12:35:55 +0000

    Finished the TextSection so it can have a point list and a headline

--- src/pages/ConceptPage.tsx
+++ src/pages/ConceptPage.tsx
@@ -22,6 +22,24 @@ const ConceptPage: FC = () => {
         highlightedText="quality"
         textAfterHighlightedText="which means you get a highly reliable system"
       />
+      <MediaSection imageUrl={FirstSubSectionImage} mediaLocation="full-screen" />
+      <TextSection
+        headline="Keywords"
+        pointList
+        textSections={[
+          'We work to build trust and to understand your business, your work and your needs in order to generate maximum value for your organisation.',
+          'We work to build trust and to understand your business, your work and your needs in order to generate maximum value for your organisation.',
+          'We offer a fixed price which is unique in the bespoke software development industry. It makes the project completely financially predictable for you and allows us to work together to create the best possible system for you instead of compromising and cutting features to stay within budget.'
+        ]}
+      />
+      <MediaSection imageUrl={FirstSubSectionImage} mediaLocation="left" />
+      <TextSection
+        textSections={[
+          'At The Moss Concept, we are on a mission to disrupt the software development industry through reuse of generic functionality.',
+          'In essence, this boils down to working smarter, not harder. This means you get quality bespoke software that has been carefully crafted to fit your workflow and your needs at a fixed price with unprecedented reliability and next to no maintenance costs.',
+          'We achieve this by spending less effort on generic aspects of the system and more on uncovering the business logic behind the software so we can ensure that it is tailored perfectly and generates as much value as possible for your organisation.'
+        ]}
+      />
     </>
   );
 };
--- src/pages/subpage/TextSection.tsx
+++ src/pages/subpage/TextSection.tsx
@@ -1,25 +1,40 @@
 import { FC } from 'react';
 import { Grid, Typography } from '@mui/material';
+import CircleIcon from '@mui/icons-material/Circle';
 
 type TextSectionProps = {
   textSections: string[];
+  headline?: string;
+  pointList?: boolean;
 };
 
-const TextSection: FC<TextSectionProps> = ({ textSections }) => {
+const TextSection: FC<TextSectionProps> = ({ textSections, headline, pointList }) => {
   return (
     <Grid container justifyContent="center" sx={{ mt: 32 }}>
-      <Grid item xs={6}>
+      {headline && (
+        <Grid item xs={12} sx={{ mb: 10 }}>
+          <Grid container justifyContent="center">
+            <Grid item xs={6}>
+              <Typography variant="TobiasSmall-h1" color="secondary.main">
+                {headline}
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      )}
+      <Grid item container xs={6}>
         {textSections.map((text) => (
-          <Typography
-            key={text}
-            color="secondary.main"
-            variant="PoppinsBig-body"
-            component="div"
-            sx={{
-              mb: 3
-            }}>
-            {text}
-          </Typography>
+          <Grid item xs={12} key={text} sx={{ mb: 3 }}>
+            {pointList && <CircleIcon color="primary" sx={{ fontSize: '5px' }} />}
+            <Typography
+              color="secondary.main"
+              variant="PoppinsBig-body"
+              component="div"
+              // The margin top is a hack to make the the dots align with the text
+              sx={pointList ? { ml: 4, marginTop: '-18px' } : undefined}>
+              {text}
+            </Typography>
+          </Grid>
         ))}
       </Grid>
     </Grid>
