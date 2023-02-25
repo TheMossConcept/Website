a71a27621d3f28dd462009f728f29ef0b50ae419
Niklas Moss <niklas@themossconcept.com>
2023-02-25 10:09:08 +0000

    Implement interactive logo on topbar

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,15 +1,14 @@
 import { Box, Grid } from '@mui/material';
-import { FC, useEffect, useState } from 'react';
-import { ReactComponent as DefaultLogo } from '../assets/Images/logo_default.svg';
+import { FC, useState } from 'react';
+import { useNavigate } from 'react-router';
+import InteractiveLogo from './interactive-icons/InteractiveLogo';
 import InteractiveMenuIcon from './interactive-icons/InteractiveMenuIcon';
 import NavigationMenu from './NavigationMenu';
 
 const Topbar: FC = () => {
   const [navigationMenuIsOpen, setNavigationMenuIsOpen] = useState(false);
-  const [opacity, setOpacity] = useState(0);
-  useEffect(() => {
-    setOpacity(100);
-  }, []);
+
+  const navigate = useNavigate();
 
   return (
     <>
@@ -17,11 +16,7 @@ const Topbar: FC = () => {
         <Grid item xs={12} sx={{ mt: 4, mx: 4 }}>
           <Grid container justifyContent="space-between">
             <Grid item>
-              <DefaultLogo
-                height={50}
-                width={106.5}
-                style={{ opacity, transition: 'opacity 630ms ease-out 920ms' }}
-              />
+              <InteractiveLogo height={50} width={106.5} onClick={() => navigate('/')} />
             </Grid>
             <Grid item>
               <InteractiveMenuIcon
