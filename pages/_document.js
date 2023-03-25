04152c87f60f8a36a0877258f46ee59e19c9cfcc
Niklas Moss <niklas@themossconcept.com>
2023-03-25 19:50:57 +0000

    Preload images and videos

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,48 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap"
           rel="stylesheet"
         />
+        <link rel="preload" href="/images/building_blocks.jpg" as="image" />
+        <link rel="preload" href="/images/business_value.jpg" as="image" />
+        <link rel="preload" href="/images/calm.jpg" as="image" />
+        <link rel="preload" href="/images/collaboration.jpg" as="image" />
+        <link rel="preload" href="/images/partnership_tall.jpg" as="image" />
+        <link rel="preload" href="/images/people_talking.jpg" as="image" />
+        <link rel="preload" href="/images/purpose_tall.jpg" as="image" />
+        <link rel="preload" href="/images/shapes.png" as="image" />
+        <link rel="preload" href="/images/trust_and_respect.jpg" as="image" />
+        <link rel="preload" href="/images/trust_and_respect_in_team.jpg" as="image" />
+        <link rel="preload" href="/images/understandability_illustration.png" as="image" />
+        <link rel="preload" href="/images/working_in_office.jpg" as="image" />
+        <link
+          rel="preload"
+          href="https://themossconcept-website-assets.fra1.cdn.digitaloceanspaces.com/working.mov"
+          as="video"
+        />
+        <link
+          rel="preload"
+          href="https://themossconcept-website-assets.fra1.cdn.digitaloceanspaces.com/frontpage.mp4"
+          as="video"
+        />
+        <link
+          rel="preload"
+          href="https://themossconcept-website-assets.fra1.cdn.digitaloceanspaces.com/building_blocks.mov"
+          as="video"
+        />
+        <link
+          rel="preload"
+          href="https://themossconcept-website-assets.fra1.cdn.digitaloceanspaces.com/trees-swaying.mov"
+          as="video"
+        />
+        <link
+          rel="preload"
+          href="https://themossconcept-website-assets.fra1.cdn.digitaloceanspaces.com/forrest.mov"
+          as="video"
+        />
+        <link
+          rel="preload"
+          href="https://themossconcept-website-assets.fra1.cdn.digitaloceanspaces.com/teamwork.mov"
+          as="video"
+        />
         <meta name="theme-color" content="#000000" />
       </Head>
       <body>
