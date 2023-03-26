5eae29917d245e37ff09fa55e66e06a204b49218
Niklas Moss <niklas@themossconcept.com>
2023-03-26 15:51:11 +0100

    Optimized rendering

--- frontpage/ConceptPageTeaserSection.tsx
+++ frontpage/ConceptPageTeaserSection.tsx
@@ -1,5 +1,5 @@
 import { Grid, Typography } from '@mui/material';
-import { FC, useRef } from 'react';
+import { FC, RefObject, useRef } from 'react';
 import TextContainer from '../components/TextContainer';
 import useAppearingText from '../utilities/useAppearingText';
 import MediaSection from '../components/sections/MediaSection';
@@ -12,12 +12,6 @@ const ConceptPageTeaserSection: FC = () => {
   const containerRef = useRef<HTMLDivElement>(null);
 
   const isMobile = useIsMobile();
-
-  const firstTextSectionOpacity = useAppearingText(containerRef, isMobile ? 50 : 82.5, 4);
-  const secondTextSectionOpacity = useAppearingText(containerRef, isMobile ? 60 : 87.5, 4);
-  const linkOpacity = useAppearingText(containerRef, isMobile ? 70 : 92.5, 4);
-
-  const router = useRouter();
 
   return (
     <Grid
@@ -33,48 +27,7 @@ const ConceptPageTeaserSection: FC = () => {
         marginBottom: { xs: 0, md: 46 },
         backgroundColor: 'secondary.main'
       }}>
-      <TextContainer>
-        <HeadlineSection
-          containerRef={containerRef}
-          marginLeft={isMobile ? undefined : '354px'}
-          firstLineText={[
-            { text: 'A', color: 'text.primary', variant: 'PoppinsBig-h1' },
-            { text: 'disruptive', color: 'text.secondary', variant: 'TobiasBig-h1' }
-          ]}
-          secondLineText={[{ text: 'concept', color: 'text.primary', variant: 'PoppinsBig-h1' }]}
-        />
-        <Typography
-          variant="PoppinsBig-subtitle2"
-          color="text.primary"
-          sx={{
-            mt: { xs: 5, md: 10 },
-            display: 'block',
-            maxWidth: '569px',
-            opacity: firstTextSectionOpacity
-          }}>
-          We are on a mission to disrupt the software development industry by cutting to the chase
-          of what is important.
-        </Typography>
-        <Typography
-          variant="PoppinsBig-subtitle2"
-          color="text.primary"
-          sx={{
-            marginTop: '34px',
-            display: 'block',
-            maxWidth: '569px',
-            opacity: secondTextSectionOpacity
-          }}>
-          We create fully custom, stable, and flexible software that can be changed and extended
-          seamlessly as the world, and the needs of your business, evolve.
-        </Typography>
-        <InteractiveLink
-          text="Read more about what makes our concept unique"
-          navigate={() => router.push('/concept')}
-          variant="PoppinsSmall-button"
-          color="text.primary"
-          sx={{ mt: { xs: 5, md: 10 }, opacity: linkOpacity }}
-        />
-      </TextContainer>
+      <ContentArea containerRef={containerRef} />
       <MediaSection
         mediaUrl="https://themossconcept-website-assets.fra1.cdn.digitaloceanspaces.com/building_blocks.mov"
         isVideo
@@ -82,6 +35,66 @@ const ConceptPageTeaserSection: FC = () => {
         marginTop={isMobile ? 0 : undefined}
       />
     </Grid>
+  );
+};
+
+type ContentAreaProps = {
+  containerRef: RefObject<HTMLDivElement>;
+};
+
+// This is a separate component to prevent excessive re-render of the background
+const ContentArea: FC<ContentAreaProps> = ({ containerRef }) => {
+  const isMobile = useIsMobile();
+
+  const firstTextSectionOpacity = useAppearingText(containerRef, isMobile ? 50 : 82.5, 3);
+  const secondTextSectionOpacity = useAppearingText(containerRef, isMobile ? 60 : 87.5, 3);
+  const linkOpacity = useAppearingText(containerRef, isMobile ? 70 : 92.5, 3);
+
+  const router = useRouter();
+
+  return (
+    <TextContainer>
+      <HeadlineSection
+        containerRef={containerRef}
+        marginLeft={isMobile ? undefined : '354px'}
+        firstLineText={[
+          { text: 'A', color: 'text.primary', variant: 'PoppinsBig-h1' },
+          { text: 'disruptive', color: 'text.secondary', variant: 'TobiasBig-h1' }
+        ]}
+        secondLineText={[{ text: 'concept', color: 'text.primary', variant: 'PoppinsBig-h1' }]}
+      />
+      <Typography
+        variant="PoppinsBig-subtitle2"
+        color="text.primary"
+        sx={{
+          mt: { xs: 5, md: 10 },
+          display: 'block',
+          maxWidth: '569px',
+          opacity: firstTextSectionOpacity
+        }}>
+        We are on a mission to disrupt the software development industry by cutting to the chase of
+        what is important.
+      </Typography>
+      <Typography
+        variant="PoppinsBig-subtitle2"
+        color="text.primary"
+        sx={{
+          marginTop: '34px',
+          display: 'block',
+          maxWidth: '569px',
+          opacity: secondTextSectionOpacity
+        }}>
+        We create fully custom, stable, and flexible software that can be changed and extended
+        seamlessly as the world, and the needs of your business, evolve.
+      </Typography>
+      <InteractiveLink
+        text="Read more about what makes our concept unique"
+        navigate={() => router.push('/concept')}
+        variant="PoppinsSmall-button"
+        color="text.primary"
+        sx={{ mt: { xs: 5, md: 10 }, opacity: linkOpacity }}
+      />
+    </TextContainer>
   );
 };
 
--- utilities/useAppearingText.ts
+++ utilities/useAppearingText.ts
@@ -8,30 +8,34 @@ const useAppearingText = (
   opacityIncreaseFactor = 1.5
 ) => {
   // This is just to be able to trigger the layout effect every time we scroll
-  const scrollY = useContext(ScrollContext);
   const [percentageOfPageVisible, setPercentageOfPageVisible] = useState<number>(0);
+  const scrollY = useContext(ScrollContext);
 
   const isMobile = useIsMobile();
   const rectifiedOpacityIncreaseFactor = isMobile
     ? opacityIncreaseFactor / 2
     : opacityIncreaseFactor;
 
-  useLayoutEffect(() => {
-    if (containerRef?.current) {
-      const containerBoundingRect = containerRef.current.getBoundingClientRect();
-      setPercentageOfPageVisible(
-        (containerBoundingRect.height / containerBoundingRect.bottom) * 100
-      );
-    }
-  }, [containerRef, scrollY]);
-
   // Don't start show anything until we hit the specified percentage of the page visible. When we do,
   // the opacity is whatever percentage of the page that is visible above that divided by 100
   // (70 - 30) / 100 = 0.4 for a page that is 70 % visible with a textVisibilityPercentage of 30.
   // times the opacity increase factor
-  return (
-    ((percentageOfPageVisible - textVisibilityPercentage) / 100) * rectifiedOpacityIncreaseFactor
-  );
+  const opacity =
+    ((percentageOfPageVisible - textVisibilityPercentage) / 100) * rectifiedOpacityIncreaseFactor;
+
+  useLayoutEffect(() => {
+    if (containerRef?.current && opacity < 1) {
+      const containerBoundingRect = containerRef.current.getBoundingClientRect();
+      const newPercentageOfPageVisible =
+        (containerBoundingRect.height / containerBoundingRect.bottom) * 100;
+
+      if (newPercentageOfPageVisible >= textVisibilityPercentage) {
+        setPercentageOfPageVisible(newPercentageOfPageVisible);
+      }
+    }
+  }, [containerRef, scrollY]);
+
+  return opacity;
 };
 
 export default useAppearingText;
