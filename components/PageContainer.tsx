2dfa005a6d0be75354f86da16949e18b49b92c24
Niklas Moss <niklas@themossconcept.com>
2023-10-22 15:18:13 +0100

    Minor fixes

--- components/PageContainer.tsx
+++ components/PageContainer.tsx
@@ -2,7 +2,6 @@ import { Grid } from '@mui/material';
 import { FC, PropsWithChildren } from 'react';
 
 const PageContainer: FC<PropsWithChildren> = ({ children }) => {
-  // TODO: The background color should come from the theme!
   return (
     <Grid container sx={{ bgcolor: 'text.secondary', maxWidth: '100vw', overflowX: 'hidden' }}>
       {children}
